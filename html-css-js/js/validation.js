f8b1e8a1f18c148298d62470fd1fbe055e04321f
wachuuu <gab.wachowski15@gmail.com>
2021-10-10 00:03:02 +0200

    validation improvements

--- html-css-js/js/validation.js
+++ html-css-js/js/validation.js
@@ -11,6 +11,8 @@ function error(input, message) {
 
 function success(input) {
   input.className = 'success';
+  const elem = input.parentNode.querySelector("small");
+  elem.innerText = '';
 }
 
 function validateInput(input, errorMsg) {
@@ -25,7 +27,7 @@ function validateInput(input, errorMsg) {
 
 function validateEmail(input, errorMsg, requiredMsg) {
   if (!hasValue(input)) {
-    error(requiredMsg);
+    error(input, requiredMsg);
     return false;
   } else {
     const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
@@ -49,7 +51,11 @@ function validateCheckboxes(input, errorMsg) {
     const elem = document.getElementById('checkbox-error');
     elem.innerText = errorMsg;
     return false;
-  } else return true;
+  } else {
+    const elem = document.getElementById('checkbox-error');
+    elem.innerText = '';
+    return true;
+  }
 }
 
 const form = document.forms[0];
@@ -66,7 +72,7 @@ form.addEventListener("submit", (event) => {
   let isFirstNameValid = validateInput(form.elements["first-name"], 'Proszę podać imię');
   let isLastNameValid = validateInput(form.elements["last-name"], 'Proszę podać nazwisko');
   let isEmailValid = validateEmail(form.elements["email"], "Nieprawidłowy adres email", "Proszę podać adres email");
-  let isAnyCheckboxSet = validateCheckboxes(checkboxes, 'Proszę zaznaczyc co najmneij jedną sekcję')
+  let isAnyCheckboxSet = validateCheckboxes(checkboxes, 'Proszę zaznaczyc co najmniej jedną sekcję')
 
   if (isFirstNameValid && isLastNameValid && isEmailValid && isAnyCheckboxSet) {
     alert("Dziękujemy za przesłanie formularza. A tak na prawdę to nie.");
